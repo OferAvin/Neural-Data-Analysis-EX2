55eb8bfe7c7474500596647f7d6b70ef328f0924
Ofer Avin <oferavin32@gmail.com>
2020-04-22 17:30:40 +0300

    removed one loop

--- ex_313328924.m
+++ ex_313328924.m
@@ -5,6 +5,7 @@ load SpikesX10U12D.mat
 num_of_neurons = size(SpikesX10U12D,1);
 num_of_degs = size(SpikesX10U12D,2);
 num_of_rep = size(SpikesX10U12D,3);
+%% creating parameters
 bin_duration = 0.02;
 times_bins_vec = (0:bin_duration:1.28);
 chosen_neuron = 3;
@@ -12,20 +13,19 @@ num_of_plots_per_raws = 2;
 num_of_bins = length(times_bins_vec);
 mat = zeros(num_of_neurons,num_of_degs,num_of_rep,num_of_bins-1);
 rate = zeros(num_of_degs,num_of_bins-1);
+onesVec = ones(1,num_of_rep);          %using for addition
+
 %% creating a 4dim array 
 for i = 1:num_of_neurons
     for j = 1:num_of_degs
         for k = 1:num_of_rep
             mat(i,j,k,:) = histcounts(SpikesX10U12D(i,j,k).TimeList,times_bins_vec);
         end
-    end
-end
 %% calculating rate for each degree for a chosen neuron
-onesVec = ones(1,num_of_rep);
-for i = 1:num_of_degs
-    rep_bin_mat = squeeze(mat(chosen_neuron,i,:,:));
-    sum_of_all_bins = onesVec*rep_bin_mat;
-    rate(i,:) = sum_of_all_bins/(bin_duration*num_of_rep);
+        rep_bin_mat = squeeze(mat(chosen_neuron,i,:,:));
+        sum_of_all_bins = onesVec*rep_bin_mat;
+        rate(i,:) = sum_of_all_bins/(bin_duration*num_of_rep);
+    end
 end
 %% Ploting
 figure();
